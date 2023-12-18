8943533cfc3dc9ceec8898be77377a022a5bcbef
StillABeginnerr <shivamagarwaloff@gmail.com>
2023-12-19 00:38:58 +0530

    done

--- src/components/Portfolio/portfolio.jsx
+++ src/components/Portfolio/portfolio.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react'
 import './portfolio.css'
-import IMG1 from '../../assets/Profile_Photo.jpeg';
-import IMG6 from '../../assets/img6.png';
+import IMG1 from '../../assets/SafeNav.png';
+import IMG2 from '../../assets/crypto-dashboard.png'
+import IMG3 from '../../assets/img6.png';
 
 const Portfolio = () => {
   return (
@@ -16,66 +17,32 @@ const Portfolio = () => {
             <img src={IMG1} alt='image' />
           </div>
         
-          <h3>Portfolio-title </h3>
+          <h3>Safe-Nav : Providing Safe Routes For Women & Solo Travellers Safety</h3>
           <div className='portfolio__item-cta'>
-          <a href='https://github.com' className='btn' target='_blank' rel="noreferrer">Github</a>
-          <a href='https://Leetcode.com' className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+          <a href='https://github.com/StillAbeginnerr/SafeNav' className='btn' target='_blank' rel="noreferrer">Github</a>
+          <a href='https://www.youtube.com/watch?v=JMakJampLWQ' className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
           </div>
          
         </article>
+      
+     
         <article className='portfolio__item'>
           <div className='portfolio__item-image'>
-            <img src={IMG1} alt='image' />
+            <img src={IMG2} alt='image' />
           </div>
-
-          <h3>Portfolio-title</h3>
-          <div className='portfolio__item-cta'>
-          <a href='https://github.com' className='btn' target='_blank' rel="noreferrer">Github</a>
-          <a href='https://Leetcode.com' className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
-          </div>
-         
-        </article>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt='image' />
-          </div>
-
-          <h3>Portfolio-title</h3>
-          <div className='portfolio__item-cta'>
-          <a href='https://github.com' className='btn' target='_blank' rel="noreferrer">Github</a>
-          <a href='https://Leetcode.com' className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
-          </div>
-         
-        </article>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt='image' />
-          </div>
-          
-          <h3>Portfolio-title</h3>
-          <div className='portfolio__item-cta'>
-          <a href='https://github.com' className='btn' target='_blank' rel="noreferrer">Github</a>
-          <a href='https://Leetcode.com' className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
-          </div>
-         
-        </article>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt='image' />
-          </div>
-           <h3>Portfolio-title</h3>
+           <h3>CryptoDashboard- Admin Dashboard for buying/Selling of Crypto</h3>
            <div className='portfolio__item-cta'>
-          <a href='https://github.com' className='btn' target='_blank' rel="noreferrer">Github</a>
-          <a href='https://Leetcode.com' className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+          <a href='https://github.com/StillAbeginnerr/Crypto-dashboard' className='btn' target='_blank' rel="noreferrer">Github</a>
+          <a href='#' className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
           </div>
          
         </article>
         <article className='portfolio__item'>
           <div className='portfolio__item-image'>
-            <img src={IMG6} alt='image' />
+            <img src={IMG3} alt='image' />
           </div>
       
-          <h3>Weather Web Application using HTML, CSS and JS with Fetch API</h3>
+          <h3>WalkingBookFairs.com : A Famous Bookstore Website</h3>
           <br></br>
           <div className='portfolio__item-cta'>
           <a href='https://github.com/StillAbeginnerr/WeatherWeb' className='btn' target='_blank' rel="noreferrer">Github</a>
